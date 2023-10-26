97a330fc767888b9c969763793f73399976ecad8
012rashidkp <fenzimate@gmail.com>
2023-10-26 18:12:27 +0530

    image upload fixed

---
+++ src/main/kotlin/mystore/net/Constants/ValidationConstant.kt
@@ -0,0 +1,25 @@
+package mystore.net.Constants
+
+import org.jetbrains.exposed.sql.statements.api.ExposedBlob
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.regex.Pattern
+
+fun isEmailValid(email: String): Boolean {
+    val regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
+    val pattern = Pattern.compile(regex)
+    val matcher = pattern.matcher(email)
+    return matcher.matches()
+}
+
+fun isPhoneNumberValid(phoneNumber: String): Boolean {
+    val regex = "^(\\+91[\\-\\s]?)?[0]?(91)?[789]\\d{9}\$"
+    val pattern = Pattern.compile(regex)
+    val matcher = pattern.matcher(phoneNumber)
+    return matcher.matches()
+}
+fun isPasswordLengthAtLeast6(password: String): Boolean {
+    return password.length >= 6
+}
+
+
